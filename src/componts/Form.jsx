cd89a5c4be9f0c57031dcd7ee52c377e526b1497
Levim-MX <mahmoodjamal54@gmail.com>
2024-12-08 16:40:59 +0300

    second form commit

--- src/componts/Form.jsx
+++ src/componts/Form.jsx
@@ -50,7 +50,7 @@ const Form = () => {
   return (
     <section className="form-container">
         
-  <form onSubmit={handleSubmit}>
+  {/* <form onSubmit={handleSubmit}>
     <div className="form-header">
       <h1>معلومات التسجيل الشخصية</h1>
     </div>
@@ -98,7 +98,73 @@ const Form = () => {
         <p>سيتم خزن وتشفير جميع البيانات لضمان السرية </p>
       <button type="submit">التالي</button>
     </div>
-  </form>
+  </form> */}
+
+
+
+ <form >
+      {/* form heading */}
+  <div className='form-header '>
+         <h1> معلومات المركبة </h1>
+  </div>
+        
+        <div className='form-body'>
+
+           {/* input1 */}
+          <div className='form-group'>
+            <select name="" id="carTyp" required>
+              <option value="">اختر نوع المركبة</option>
+              <option value="AUDI A3"> أودي A3</option>
+              <option value="AUDI A4"> AUDI A4</option>
+              <option value="AUDI A6">AUDI A6  </option>
+              <option value="AUDI A8">  AUDI A8</option>
+              <option value="AUDI Q3"> AUDI Q3  </option>
+              <option value="AUDI Q5"> AUDI Q5 </option>
+              <option value="AUDI Q7"> AUDI Q7  </option>
+              <option value="AUDI Q8"> AUDI Q8  </option>
+              <option value="AUDI TT"> AUDI TT </option>
+              <option value="AUDI R8"> AUDI R8  </option>
+              
+          
+            </select>
+
+          </div>
+
+          {/* input2 */}
+          <div className='form-group'>
+          <select name="" id="carServise" required>
+              <option value="">اختر نوع الخدمة</option>
+              <option value="Oil Change"> تغير زيت السيارة </option>
+              <option value="ُEngin Maintance"> صيانة المحرك</option>
+              <option value="Bistem ">صيانة بستم المحرك  </option>
+              <option value="Car Washing">  غسل وتنضيف المركبة</option>
+              <option value="Car Breaks ">  صيانة المكابح  </option>
+              <option value="Car Wheel ">  صيانة الاطارات   </option>
+            
+              
+          
+            </select>
+
+          </div>
+           {/* input3 */}
+          <div className='form-group'>
+
+          </div> 
+           {/* input4*/}
+          <div className='form-group'>
+
+          </div>
+          
+        </div>
+          
+
+
+
+
+ </form>
+
+
+
 </section>
 
   );
